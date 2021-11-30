79bcfbe38ec726ce4c13a41b962a531bb0546844
poiupoiu96 <poiupoiu96@naver.com>
2021-12-01 00:53:59 +0900

    컴포넌트, 반복문, state

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,12 +3,16 @@ import React, { useState } from 'react';
 
 function App() {
   let textColor = {color: 'blue', fontSize: '40px'}
-  let [ title1, titleUpdate1 ] = useState([{area:'인천', date: '1993-03-01'}, 
-                                            {area:'서울', date: '1994-02-20'},
-                                            {area:'여수', date: '2021-10-01'}]);
-  let [likeNum, likeNumUpdate] = useState(0)         
+  let [ title1, titleUpdate1 ] = useState([{area:'인천', date: '1993-03-01', num: 0}, 
+                                            {area:'서울', date: '1994-02-20', num: 0},
+                                            {area:'여수', date: '2021-10-01', num: 0}]);
+  let [likeNum, likeNumUpdate] = useState( [0] )         
   let [flag, flagUpdate] = useState(false)                                
-  
+  let [detail, detailUpdate] = useState( [{title: '인천맛집', date: '2021-08-07', text:'인천어디어디어디'},
+                                          {title: '서울맛집', date: '2021-06-11', text:'서울어디어디어디'},
+                                          {title: '여수맛집', date: '2021-02-07', text:'여수어디어디어디'}])
+
+  let [clickNum, clickNumUpdate] = useState(0)                                          
   // state를 사용해서 내용 바꿔보기
   // let [chngeWord, chngeWordUpdate] = useState(['강릉', '부산', '목포'])
 
@@ -19,14 +23,14 @@ function App() {
   //   tempList[2].area = chngeWord[2]
   //   titleUpdate1(tempList)
   // }
-
+ 
   return (
     <div className="App">
       <div className="black-nav" style={textColor}>
         <div>REACT BLOG</div>
       </div>
       {/* <button onClick={ changeWord }> 글씨 바꾸기 Btn</button> */}
-      <div className="list" onClick={ () => { flagUpdate(!flag) } }>
+      {/* <div className="list" onClick={ () => { flagUpdate(!flag) } }>
         <h3 > { title1[0].area } <span onClick={ () => { likeNumUpdate(likeNum + 1) }}>👍</span> { likeNum } </h3> 
         <p> { title1[0].date } </p>
         <hr/>
@@ -40,7 +44,22 @@ function App() {
         <h3> { title1[2].area } </h3>
         <p> { title1[2].date } </p>
         <hr/>
-      </div>
+      </div> */}
+
+      {/* 반복문 연습 map */}
+      {
+        title1.map((item, idx) => {
+          return (
+            // <div onClick={ () => { clickNumUpdate(idx) }}>
+            <div className="list" onClick={ () => { flagUpdate(!flag) }}>
+              <h3> { item.area } <span onClick={ () => { likeNumUpdate(likeNum + 1) }}>👍</span> { likeNum[idx] } </h3> 
+              <p> { item.date } </p>
+              <hr/>
+            </div>
+            // </div>
+          )
+        })
+      }
 
       {/* 컴포넌트로 만들기 */}
       {/* <div className="modal">
@@ -48,21 +67,24 @@ function App() {
         <p>날짜</p>
         <p>상세내용</p>
         </div> */}
-
       {
-        flag === true ? <Modal></Modal> : null
+        flag === true ? <Modal detail = {detail} clickNum = {clickNum}></Modal> : null
       }
+
+      {/* props로 하위 컴포넌트에 전달방법  */}
+      {/* <Modal 작명 = { 전송할 state }></Modal> */}
+      
     </div>
   );
 }
 
-function Modal () {
+function Modal (param) {
   return (
     // git
     <div className="modal">
-      <h2>제목</h2>
-      <p>날짜</p>
-      <p>상세내용</p>
+      <h2>제목  { param.detail[param.clickNum].title }</h2>
+      <p>날짜 { param.detail[param.clickNum].date }</p>
+      <p>상세내용 { param.detail[param.clickNum].text }</p>
   </div>
   )
 }
